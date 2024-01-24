8fae0c8113845e784b1b3cfd7f437aec3c8e73e7
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-01-24 11:00:54 +0200

    Completed The Footer Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Header />
             <Hero />
             <Main />
+            <Footer />
         </>
     )
 }
